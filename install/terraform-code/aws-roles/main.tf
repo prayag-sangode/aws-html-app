86eb68fa799d300947138873053ab7fb59a5a31e
Prayag Sangode <prayag.rhce@gmail.com>
2024-09-07 10:08:23 +0000

    udpate code

--- install/terraform-code/aws-roles/main.tf
+++ install/terraform-code/aws-roles/main.tf
@@ -47,6 +47,13 @@ resource "aws_iam_policy" "codebuild_policy" {
           "logs:PutLogEvents"
         ],
         Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "eks:DescribeCluster"
+        ],
+        Resource = "arn:aws:eks:us-east-1:123456789012:cluster/my-cluster" # Replace with your EKS cluster ARN
       }
     ]
   })
@@ -89,13 +96,6 @@ resource "aws_iam_policy" "codepipeline_policy" {
       {
         Effect = "Allow",
         Action = [
-          "codedeploy:CreateDeployment",
-          "codedeploy:GetDeployment",
-          "codedeploy:UpdateDeploymentGroup",
-          "codedeploy:DeleteDeployment",
-          "codedeploy:GetDeploymentConfig",
-          "codedeploy:GetDeploymentGroup",
-          "codedeploy:ListDeployments",
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
@@ -125,61 +125,6 @@ resource "aws_iam_role_policy_attachment" "codepipeline_ecr_policy" {
 resource "aws_iam_role_policy_attachment" "codepipeline_codebuild_policy" {
   role       = aws_iam_role.codepipeline_role.name
   policy_arn  = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
-}
-
-# IAM Role for CodeDeploy
-resource "aws_iam_role" "codedeploy_role" {
-  name = "CodeDeployRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "codedeploy.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# IAM Policy for CodeDeploy
-resource "aws_iam_policy" "codedeploy_policy" {
-  name        = "CodeDeployPolicy"
-  description = "Policy for CodeDeploy operations"
-  policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "codedeploy:CreateDeployment",
-          "codedeploy:GetDeployment",
-          "codedeploy:UpdateDeploymentGroup",
-          "codedeploy:DeleteDeployment",
-          "codedeploy:GetDeploymentConfig",
-          "codedeploy:GetDeploymentGroup",
-          "codedeploy:ListDeployments"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ],
-        Resource = "arn:aws:s3:::your-s3-bucket/*" # Replace with your S3 bucket ARN
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codedeploy_policy_attachment" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn  = aws_iam_policy.codedeploy_policy.arn
 }
 
 # Outputs
@@ -231,19 +176,4 @@ output "codepipeline_codebuild_policy_arn" {
 output "codepipeline_codebuild_policy_name" {
   description = "The name of the CodeBuild Admin Access policy for CodePipeline"
   value       = "AWSCodeBuildAdminAccess"
-}
-
-output "codedeploy_role_arn" {
-  description = "The ARN of the CodeDeploy role"
-  value       = aws_iam_role.codedeploy_role.arn
-}
-
-output "codedeploy_policy_arn" {
-  description = "The ARN of the CodeDeploy policy"
-  value       = aws_iam_policy.codedeploy_policy.arn
-}
-
-output "codedeploy_policy_name" {
-  description = "The name of the CodeDeploy policy"
-  value       = aws_iam_policy.codedeploy_policy.name
 }
