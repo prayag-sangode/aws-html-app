d1ca23bc1e989548a52984b50ad3c14ea7c644a3
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2025-02-11 13:24:46 +0530

    Update utilities.sh

--- install/utilities.sh
+++ install/utilities.sh
@@ -19,10 +19,12 @@ sudo apt-get install -y \
 
 # Install Docker
 echo "Installing Docker..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt -y install docker.io
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Create the Docker group if it does not exist
 if ! getent group docker > /dev/null; then
